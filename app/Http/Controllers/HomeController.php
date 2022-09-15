2c25d862ea8a3f7f04986cff2bcc8242ca12ff48
robinzmeer@hotmail.com <Raadjetochniet12@>
2022-09-15 15:26:45 +0200

    Ik heb een welkomstpagina gemaakt die via de controller wordt doorgelinkt. Ook heb ik een idee verzonnen voor een eindproject

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function show()
+    {
+        $headTitle = 'Home';
+        return view('welcome',
+        compact('headTitle'));
+    }
 }
